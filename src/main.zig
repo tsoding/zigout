a500c5b1287ef77beeb6bc1f7a65ac7854eef419
rexim <reximkut@gmail.com>
2022-05-20 08:14:23 +0700

    Implement almost all the game mechanics

--- src/main.zig
+++ src/main.zig
@@ -14,49 +14,85 @@ const BAR_LEN: f32 = 100;
 const BAR_THICCNESS: f32 = PROJ_SIZE;
 const BAR_Y: f32 = WINDOW_HEIGHT - BAR_THICCNESS - 100;
 const BAR_SPEED: f32 = PROJ_SPEED;
+const TARGET_WIDTH = BAR_LEN;
+const TARGET_HEIGHT = BAR_THICCNESS;
+const TARGET_PADDING = 20;
+const TARGETS_CAP = 128;
+
+const Target = struct {
+    x: f32,
+    y: f32,
+    dead: bool = false,
+};
+
+var targets_pool = [_]Target{
+    Target { .x = 100, .y = 100 },
+    Target { .x = 100 + TARGET_WIDTH + TARGET_PADDING, .y = 100 },
+    Target { .x = 100 + (TARGET_WIDTH + TARGET_PADDING)*2, .y = 100 },
+    Target { .x = 100 + (TARGET_WIDTH + TARGET_PADDING)*3, .y = 100 },
+    Target { .x = 100 + (TARGET_WIDTH + TARGET_PADDING)*4, .y = 100 },
+};
 
 var quit = false;
-var proj_x:  f32 = 100;
-var proj_y:  f32 = 100;
+var bar_x:   f32 = WINDOW_WIDTH/2 - BAR_LEN/2;
+var bar_dx:  f32 = 0;
+var proj_x:  f32 = WINDOW_WIDTH/2 - PROJ_SIZE/2;
+var proj_y:  f32 = BAR_Y - BAR_THICCNESS/2 - PROJ_SIZE;
 var proj_dx: f32 = 1;
 var proj_dy: f32 = 1;
-var bar_x:   f32 = 0;
-var bar_dx:  f32 = 0;
 var pause = false;
+var started = false;
 
-fn proj_rect(x: f32, y: f32) c.SDL_Rect {
-    const rect = c.SDL_Rect {
+fn make_rect(x: f32, y: f32, w: f32, h: f32) c.SDL_Rect {
+    return c.SDL_Rect {
         .x = @floatToInt(i32, x),
         .y = @floatToInt(i32, y),
-        .w = PROJ_SIZE,
-        .h = PROJ_SIZE
+        .w = @floatToInt(i32, w),
+        .h = @floatToInt(i32, h)
     };
-    return rect;
+}
+
+fn target_rect(target: Target) c.SDL_Rect {
+    return make_rect(target.x, target.y, TARGET_WIDTH, TARGET_HEIGHT);
+}
+
+fn proj_rect(x: f32, y: f32) c.SDL_Rect {
+    return make_rect(x, y, PROJ_SIZE, PROJ_SIZE);
 }
 
 fn bar_rect() c.SDL_Rect {
-    const rect = c.SDL_Rect {
-        .x = @floatToInt(i32, bar_x),
-        .y = @floatToInt(i32, BAR_Y - BAR_THICCNESS/2),
-        .w = @floatToInt(i32, BAR_LEN),
-        .h = @floatToInt(i32, BAR_THICCNESS),
-    };
-    return rect;
+    return make_rect(bar_x, BAR_Y - BAR_THICCNESS/2, BAR_LEN, BAR_THICCNESS);
 }
 
 fn update(dt: f32) void {
-    if (!pause) {
+    if (!pause and started) {
         bar_x = math.clamp(bar_x + bar_dx*BAR_SPEED*dt, 0, WINDOW_WIDTH - BAR_LEN);
 
         var proj_nx = proj_x + proj_dx*PROJ_SPEED*dt;
-        if (proj_nx < 0 or proj_nx + PROJ_SIZE > WINDOW_WIDTH or c.SDL_HasIntersection(&proj_rect(proj_nx, proj_y), &bar_rect()) != 0) {
+        var cond_x = proj_nx < 0 or proj_nx + PROJ_SIZE > WINDOW_WIDTH or c.SDL_HasIntersection(&proj_rect(proj_nx, proj_y), &bar_rect()) != 0;
+        for (targets_pool) |*target| {
+            if (cond_x) break;
+            if (!target.dead) {
+                cond_x = cond_x or c.SDL_HasIntersection(&proj_rect(proj_nx, proj_y), &target_rect(target.*)) != 0;
+                if (cond_x) target.dead = true;
+            }
+        }
+        if (cond_x) {
             proj_dx *= -1;
             proj_nx = proj_x + proj_dx*PROJ_SPEED*dt;
         }
         proj_x = proj_nx;
 
         var proj_ny = proj_y + proj_dy*PROJ_SPEED*dt;
-        if (proj_ny < 0 or proj_ny + PROJ_SIZE > WINDOW_HEIGHT or c.SDL_HasIntersection(&proj_rect(proj_x, proj_ny), &bar_rect()) != 0) {
+        var cond_y = (proj_ny < 0 or proj_ny + PROJ_SIZE > WINDOW_HEIGHT or c.SDL_HasIntersection(&proj_rect(proj_x, proj_ny), &bar_rect()) != 0);
+        for (targets_pool) |*target| {
+            if (cond_y) break;
+            if (!target.dead) {
+                cond_y = cond_y or c.SDL_HasIntersection(&proj_rect(proj_x, proj_ny), &target_rect(target.*)) != 0;
+                if (cond_y) target.dead = true;
+            }
+        }
+        if (cond_y) {
             proj_dy *= -1;
             proj_ny = proj_y + proj_dy*PROJ_SPEED*dt;
         }
@@ -70,6 +106,13 @@ fn render(renderer: *c.SDL_Renderer) void {
 
     _ = c.SDL_SetRenderDrawColor(renderer, 0xFF, 0, 0, 0xFF);
     _ = c.SDL_RenderFillRect(renderer, &bar_rect());
+
+    _ = c.SDL_SetRenderDrawColor(renderer, 0, 0xFF, 0, 0xFF);
+    for (targets_pool) |target| {
+        if (!target.dead) {
+            _ = c.SDL_RenderFillRect(renderer, &target_rect(target));
+        }
+    }
 }
 
 pub fn main() !void {
@@ -111,9 +154,11 @@ pub fn main() !void {
         bar_dx = 0;
         if (keyboard[c.SDL_SCANCODE_A] != 0) {
             bar_dx += -1;
+            started = true;
         }
         if (keyboard[c.SDL_SCANCODE_D] != 0) {
             bar_dx += 1;
+            started = true;
         }
 
         update(DELTA_TIME_SEC);
