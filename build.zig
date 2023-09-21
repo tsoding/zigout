3ec86c555433fabf88f3e30bb6ebf863fad1981e
HoussemBousmaha <ky_bousmaha@esi.dz>
2023-09-21 05:28:46 +0300

    fix executable name

--- build.zig
+++ build.zig
@@ -5,7 +5,7 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
     const exe = b.addExecutable(
         .{
-            .name = "playground",
+            .name = "zigout",
             .root_source_file = .{ .path = "src/main.zig" },
             .target = target,
             .optimize = optimize,
